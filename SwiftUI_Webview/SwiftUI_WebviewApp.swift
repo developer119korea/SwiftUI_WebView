f3ceac091d25d5f886353c46494768a2947f4ac4
Youngwan Cho <developer119korea@gmail.com>
2022-05-10 17:07:22 +0900

    Initial Commit

---
+++ SwiftUI_Webview/SwiftUI_WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WebviewApp.swift
+//  SwiftUI_Webview
+//
+//  Created by Youngwan Cho on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
